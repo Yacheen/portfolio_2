0aea7a9ae4dfe89b1f78eb2c69831cd70db0cca6
Yassin <81421290+Yacheen@users.noreply.github.com>
2022-05-05 21:33:54 -0600

    added the hero section

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export const HeroSection = () => {
+	return (
+		<div className="hero-section">
+			<div clasName="hero-section-content">
+				<div className="hero-about">
+					<h1>Yassin Moussaoui</h1>
+					<h2>Software Engineer</h2>
+					<p>
+						I enjoy creating mobile-responsive solutions for great
+						user experiences. -Nice to meet you
+					</p>
+				</div>
+
+				<div className="hero-yassincodes-project">
+					<div className="hero-yassincodes-description">
+						<h3>
+							Custom Twitch chat authenticated via Twitch oAuth
+						</h3>
+						<button type="button">View</button>
+					</div>
+
+					<div className=""></div>
+				</div>
+			</div>
+		</div>
+	);
+};
