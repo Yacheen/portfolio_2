dcecc266b8e4a6a7c0f9427110bdf5c85f855f7f
Yassin Moussaoui <yassinmoussaouibusiness@gmail.com>
2022-05-07 13:30:27 -0600

    form now properly sends emails to me

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Navbar from "./components/Navbar";
 import { HeroSection } from "./components/HeroSection";
 import { ProjectsSection } from "./components/ProjectsSection";
 import { Footer } from "./components/Footer";
+import {Contact} from "./components/Contact";
 function App() {
 	return (
 		<>
 			<Navbar />
 			<HeroSection />
 			<ProjectsSection />
+			<Contact />
 			<Footer />
 		</>
 	);
