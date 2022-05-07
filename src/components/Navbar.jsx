590097bcd5517a52f0b8dfa206c23c0d7872bb4c
Yassin <81421290+Yacheen@users.noreply.github.com>
2022-05-07 07:16:43 -0600

    added some navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import Switch from "@mui/material/Switch";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 
 export const Navbar = () => {
+	const [darkMode, setDarkMode] = useState(false);
 	return (
 		<div className="navbar">
 			<div className="navbar-content">
 				<h2 id="#About">Yassin's Portfolio</h2>
+				<div className="light-switch">
+					<Switch
+						value="yo"
+						onClick={() => setDarkMode(!darkMode)}
+						label="Dis The end"
+						labelPlacement="end"
+						color="secondary"
+					/>
+					<p>
+						{darkMode ? (
+							<LightModeOutlinedIcon fontSize="large" />
+						) : (
+							<DarkModeOutlinedIcon fontSize="large" />
+						)}
+					</p>
+				</div>
 				<ul className="nav-links">
 					<a href="#Projects">
 						<li className="link">Projects</li>
